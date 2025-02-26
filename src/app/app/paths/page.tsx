2e6f23305271f9ed832376a285f6b166e6a0f427
Philipe Morais <liperapltda@gmail.com>
2025-02-26 19:56:48 -0300

    delete files for update techs

--- src/app/app/paths/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function Paths() {
-  return (
-    <div>
-
-    </div>
-  )
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,176 +1,62 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
-const config: Config = {
-  darkMode: ['class'],
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+export default {
+    darkMode: ["class"],
+    content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        text: {
-          DEFAULT: 'hsl(var(--text-primary))',
-          secondary: 'hsl(var(--text-secondary))',
-          tertiary: 'hsl(var(--text-tertiary))',
-        },
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          light: 'hsl(var(--primary-light))',
-          dark: 'hsl(var(--primary-dark))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          light: 'hsl(var(--secondary-light))',
-          dark: 'hsl(var(--secondary-dark))',
-        },
-        tertiary: 'hsl(var(--tertiary))',
-        quaternary: 'hsl(var(--quaternary))',
-        quinary: 'hsl(var(--quinary))',
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        success: {
-          DEFAULT: 'hsl(var(--success))',
-          foreground: 'hsl(var(--success-foreground))',
-        },
-        warning: {
-          DEFAULT: 'hsl(var(--warning))',
-          foreground: 'hsl(var(--warning-foreground))',
-        },
-        border: 'hsl(var(--border))',
-        borderButton: 'hsl(var(--border-button))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))',
-        },
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      keyframes: {
-        'accordion-down': {
-          from: {
-            height: '0',
-          },
-          to: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-        },
-        'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-          to: {
-            height: '0',
-          },
-        },
-        'shimmer-slide': {
-          to: {
-            transform: 'translate(calc(100cqw - 100%), 0)',
-          },
-        },
-        'spin-around': {
-          '0%': {
-            transform: 'translateZ(0) rotate(0)',
-          },
-          '15%, 35%': {
-            transform: 'translateZ(0) rotate(90deg)',
-          },
-          '65%, 85%': {
-            transform: 'translateZ(0) rotate(270deg)',
-          },
-          '100%': {
-            transform: 'translateZ(0) rotate(360deg)',
-          },
-        },
-        ripple: {
-          '0%, 100%': {
-            transform: 'translate(-50%, -50%) scale(1)',
-          },
-          '50%': {
-            transform: 'translate(-50%, -50%) scale(0.9)',
-          },
-        },
-        moveHorizontal: {
-          '0%': {
-            transform: 'translateX(-50%) translateY(-10%)',
-          },
-          '50%': {
-            transform: 'translateX(50%) translateY(10%)',
-          },
-          '100%': {
-            transform: 'translateX(-50%) translateY(-10%)',
-          },
-        },
-        moveInCircle: {
-          '0%': {
-            transform: 'rotate(0deg)',
-          },
-          '50%': {
-            transform: 'rotate(180deg)',
-          },
-          '100%': {
-            transform: 'rotate(360deg)',
-          },
-        },
-        moveVertical: {
-          '0%': {
-            transform: 'translateY(-50%)',
-          },
-          '50%': {
-            transform: 'translateY(50%)',
-          },
-          '100%': {
-            transform: 'translateY(-50%)',
-          },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        'shimmer-slide':
-          'shimmer-slide var(--speed) ease-in-out infinite alternate',
-        'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear',
-        ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite',
-        first: 'moveVertical 30s ease infinite',
-        second: 'moveInCircle 20s reverse infinite',
-        third: 'moveInCircle 40s linear infinite',
-        fourth: 'moveHorizontal 40s ease infinite',
-        fifth: 'moveInCircle 20s ease infinite',
-      },
-      fontFamily: {
-        coolvetica: ['Coolvetica', 'Inter', 'sans-serif'],
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [require('tailwindcss-animate')],
-}
-export default config
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
